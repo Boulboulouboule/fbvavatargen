f584bfc13f42d81a59aa90df4282a20e9afc5da5
Boulboulouboule <vincent.pierrat@gmail.com>
2017-11-29 11:31:30 +0100

    Wrap code with try catch

--- merge.php
+++ merge.php
@@ -1,31 +1,36 @@
 <?php
-$ffmpeg = FFMpeg\FFMpeg::create();
+try {
+    $ffmpeg = FFMpeg\FFMpeg::create();
 
 // Réglage nom/durée de la video finale
-$date = new \DateTime();
-$filename = $_GET['file'] ?? $date->getTimestamp();
-$duration = 2;
+    $date = new \DateTime();
+    $filename = $_GET['file'] ?? $date->getTimestamp();
+    $duration = 2;
 
-if ($imgPath = $_GET['file']) {
+    if ($imgPath = $_GET['file']) {
 // Créer une video à partie d'une image (reglage de la durée au dessus)
-    echo exec('ffmpeg -loop 1 -i ' . $imgPath . ' -c:v libx264 -t ' . $duration . ' -pix_fmt yuv720p -vf scale=1280:720 temp1.mp4');
-} else {
-    http_response_code(404);
-    echo 'Il manque l\'image dans la requête';
-}
+        echo exec('ffmpeg -loop 1 -i ' . $imgPath . ' -c:v libx264 -t ' . $duration . ' -pix_fmt yuv720p -vf scale=1280:720 temp1.mp4');
+    } else {
+        http_response_code(404);
+        echo 'Il manque l\'image dans la requête';
+    }
 
-$bmwVideo = $ffmpeg->open('./videos/bmw.mp4');
-$bmwVideo
-    ->filters()
-    ->resize(new FFMpeg\Coordinate\Dimension(1280, 720))
-    ->synchronize();
+    $bmwVideo = $ffmpeg->open('./videos/bmw.mp4');
+    $bmwVideo
+        ->filters()
+        ->resize(new FFMpeg\Coordinate\Dimension(1280, 720))
+        ->synchronize();
 
-$bmwVideo
-    ->save(new FFMpeg\Format\Video\X264(), 'temp2.mp4');
+    $bmwVideo
+        ->save(new FFMpeg\Format\Video\X264(), 'temp2.mp4');
 
 // Fusionne les 2 videos temporaires
-echo exec('ffmpeg -f concat -i temp*.mp4 -c copy ./videos/' . $filename . '.mp4');
+    echo exec('ffmpeg -f concat -i temp*.mp4 -c copy ./videos/' . $filename . '.mp4');
 
 // Suppression des videos temporaires
-unlink('temp1.mp4');
-unlink('temp2.mp4');
+    unlink('temp1.mp4');
+    unlink('temp2.mp4');
+} catch(Exception $e) {
+    echo $e->getMessage();
+    echo $e->getTrace();
+}
