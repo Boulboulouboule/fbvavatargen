7293aee1103640e6a8df4969f47488b9f6cf791f
Boulboulouboule <vincent.pierrat@gmail.com>
2017-11-29 11:43:51 +0100

    Test

--- merge.php
+++ merge.php
@@ -10,10 +10,10 @@ try {
 
 // Réglage nom/durée de la video finale
     $date = new \DateTime();
-    $filename = $_GET['file'] ?? $date->getTimestamp();
+    $filename = 'newvideo';
     $duration = 2;
 
-    if ($imgPath = $_GET['file']) {
+    if ($imgPath = './avatars/20171129080646.jpg') {
 // Créer une video à partie d'une image (reglage de la durée au dessus)
         echo exec('ffmpeg -loop 1 -i ' . $imgPath . ' -c:v libx264 -t ' . $duration . ' -pix_fmt yuv720p -vf scale=1280:720 temp1.mp4');
     } else {
@@ -34,8 +34,8 @@ try {
     echo exec('ffmpeg -f concat -i temp*.mp4 -c copy ./videos/' . $filename . '.mp4');
 
 // Suppression des videos temporaires
-    unlink('temp1.mp4');
-    unlink('temp2.mp4');
+//    unlink('temp1.mp4');
+//    unlink('temp2.mp4');
 } catch(Exception $e) {
     echo $e->getMessage();
     echo $e->getTrace();
